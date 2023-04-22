cedcc2b28f8ab5ddfdca3c27766a1cc0482045b0
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-23 01:13:55 +0530

    Add files via upload

---
+++ leetcode/findTwice.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int findDuplicate(int arr[],int n) 
+{
+   int z=0;
+   for(int i=0;i<n;i++){
+       cout<<"true";
+       }
+   
+	
+}
+int main(){
+    
+    int size;
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+
+    // int arr[n]={1,2,3,4,4};
+    findDuplicate(arr,size);
+}
